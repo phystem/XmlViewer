b9902b444f262a8be0ae21304d2f4b59d9e9c8b2
Madhanraj <phystem2@gmail.com>
2016-09-12 01:47:32 +0530

    Changed Sun import to java import

--- src/xmlviewer/XmlUI.java
+++ src/xmlviewer/XmlUI.java
@@ -5,12 +5,12 @@
  */
 package xmlviewer;
 
-import com.sun.glass.events.KeyEvent;
 import java.awt.Toolkit;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
